34df42150babb50563a3741c9735e6f571bcd0b3
Rasmus Selsmark <rasmus@unity3d.com>
2019-05-17 17:54:48 +0300

    Shared Jenkins pipeline

---
+++ README.md
@@ -0,0 +1,3 @@
+# shared-jenkinslib-example
+
+Based on https://jenkins.io/doc/book/pipeline/shared-libraries, used for workshop/demo of Jenkins pipeline
---
+++ vars/Pipeline.groovy
@@ -0,0 +1,23 @@
+def call(body) {
+
+  service = 'echo $JOB_NAME | cut -d \"/\" -f 2'.execute().text().trim()
+  revision = 'git log --format=\"%H\" -n 1']).execute().trim()
+  image = "gcr.io/unity-ads-workshop-test/${service}/${revision}"
+
+  echo "service: ${service}"
+  echo "revision: ${revision}"
+
+  stage("build") {
+    withEnv([
+      "image=${image}",
+    ]) {
+      "sh/build.sh".execute()
+    }
+  }
+  stage("test") {
+    echo "test..."
+  }
+  stage("deploy") {
+    echo "deploy..."
+  }
+}
