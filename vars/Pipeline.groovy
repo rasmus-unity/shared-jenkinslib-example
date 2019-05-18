a8236e054cdffa0b525a00e05cb7d5cee9124908
Rasmus Selsmark <rasmus@unity3d.com>
2019-05-19 01:31:27 +0300

    Added node() context

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -1,23 +1,25 @@
 def call(body) {
 
-  service = sh([returnStdout: true, script: 'echo $JOB_NAME | cut -d \"/\" -f 2']).trim()
-  // revision = sh([returnStdout: true, script: 'git log --format=\"%H\" -n 1']).trim()
-  // image = "gcr.io/unity-ads-workshop-test/${service}:${revision}"
+  node {
+    service = sh([returnStdout: true, script: 'echo $JOB_NAME | cut -d \"/\" -f 2']).trim()
+    revision = sh([returnStdout: true, script: 'git log --format=\"%H\" -n 1']).trim()
+    image = "gcr.io/unity-ads-workshop-test/${service}:${revision}"
 
-  echo "service: ${service}"
-  // echo "revision: ${revision}"
+    echo "service: ${service}"
+    echo "revision: ${revision}"
 
-  stage("build") {
-    withEnv([
-      "image=${image}",
-    ]) {
-      "sh/build.sh".execute()
+    stage("build") {
+      withEnv([
+        "image=${image}",
+      ]) {
+        "sh/build.sh".execute()
+      }
     }
-  }
-  stage("test") {
-    echo "test..."
-  }
-  stage("deploy") {
-    echo "deploy..."
+    stage("test") {
+      echo "test..."
+    }
+    stage("deploy") {
+      echo "deploy..."
+    }
   }
 }
