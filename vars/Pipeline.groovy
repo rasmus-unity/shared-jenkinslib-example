2d96b19568809512597e67bfd06279ef291c2e58
Rasmus Selsmark <rasmus@unity3d.com>
2019-05-19 03:13:28 +0300

    use sh for executing docker + added build step comments

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -6,6 +6,7 @@ def call(body) {
     revision = sh([returnStdout: true, script: 'git log --format=\"%H\" -n 1']).trim()
     image = "gcr.io/unity-ads-workshop-test/${service}:${revision}"
 
+    // build docker image
     stage("build") {
       withEnv([
         "image=${image}",
@@ -16,13 +17,22 @@ def call(body) {
         """
       }
     }
+
+    // run tests for service
     stage("test") {
       echo "test..."
     }
+
+    // push image to docker registry, so can be pulled by Kubernetes manifests
+    stage("push image") {
+      sh "docker push ${image}"
+    }
+
+    // deploy service by applying kubernetes manifests in service repo
     stage("deploy") {
       if (fileExists("manifests")) {
-        "docker run --rm --volume `pwd`:/service gcr.io/unity-ads-workshop-test/workshop-deployer bash -c 'kubectl apply -f /service/manifests/service.yaml' -f /service/manifests/deployment.yaml'".execute()
-        "docker run --rm --volume `pwd`:/service gcr.io/unity-ads-workshop-test/workshop-deployer bash -c 'kubectl expose deployment -n workshop ${service} --type=LoadBalancer --name=${service}-lb --port=8080'".execute()
+        sh "docker run --rm --volume `pwd`:/service gcr.io/unity-ads-workshop-test/workshop-deployer bash -c 'kubectl apply -f /service/manifests/service.yaml' -f /service/manifests/deployment.yaml'"
+        sh "docker run --rm --volume `pwd`:/service gcr.io/unity-ads-workshop-test/workshop-deployer bash -c 'kubectl expose deployment -n workshop ${service} --type=LoadBalancer --name=${service}-lb --port=8080'"
       } else {
         echo "No manifests/ folder found, skipping deployment"
       }
